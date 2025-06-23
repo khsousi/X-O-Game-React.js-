74ee90bf8cc048d6aa38da7715cc720b79bd4fee
Khaled Sousi <107036069+khsousi@users.noreply.github.com>
2025-06-23 10:43:22 +0300

    Update Cell.jsx

--- src/app/Components/Cell.jsx
+++ src/app/Components/Cell.jsx
@@ -3,7 +3,7 @@ export default function Cell({go , setGo , id, cells , setCells , cell , winning
   const handleClick = (e)=>{
     if(winningMessage){
       return
-    }
+    } 
     const notTaken = !cells[id]
     if (notTaken){
       if (go === "circle"){
